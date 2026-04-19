5be8eecc08a9cc52c58a5191aeb9e5778b6e89ec
WebDesign <pharelrohit1992@gmail.com>
2026-04-19 22:09:09 +1000

    Switch from BrowserRouter to HashRouter and add ContactPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import Index from "./pages/Index.tsx";
-import NotFound from "./pages/NotFound.tsx";
-import PlanSignup from "./pages/PlanSignup.tsx";
+
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+import PlanSignup from "./pages/PlanSignup";
+import ContactPage from "./pages/ContactPage";
 
 const queryClient = new QueryClient();
 
@@ -14,13 +16,14 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/plan-signup" element={<PlanSignup />} />
+          <Route path="/contact" element={<ContactPage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
